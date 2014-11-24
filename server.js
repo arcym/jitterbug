1ecc08ed0acd25596977608f000991ad86b2e3d6
Andrew McPherson <andrewmcp333@gmail.com>
2014-11-24 18:41:22 -0500

    Attempted to use gulp to stream sass.

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-if(process.argv.length > 2)
+/*if(process.argv.length > 2)
 {
 	if(process.argv[2] == "start")
 	{
 		var forever = require("forever");
-		forever.start("server", new Object());
-		console.log("started forever");
+		forever.start("server", {});
 	}
 	else if(process.argv[2] == "stop")
 	{
 		var forever = require("forever");
 		forever.stop("server");
-		console.log("stopped forever");
 	}
 }
-else
+*/
+
+var fs = require("fs");
+var stream = require("stream");
+var gulp = require("gulp");
+var express = require("express");
+
+server = express();
+
+server.get("/*.css", function(request, response)
 {
-	express = require("express");
-	
-	server = express();
-	server.get("/", function(request, response)
-	{
-		console.log("Hello World!");
-		response.send("Hello World!");
-	});
-	server.listen(1271);
-	
-	console.log("The server is at 127.0.0.1:1271.");
-}
+	var path = "./source/" + request.params[0] + ".scss";
+	gulp.src(path).pipe(response);
+});
+
+server.listen(1271);
+
+console.log("The server is at 127.0.0.1:1271.");
