9ebe21ed363dd5476b309ae34adc4bc848fc883b
Andrew McPherson <andrewmcp333@gmail.com>
2014-11-29 08:22:03 -0500

    Began the process of bundling the module to npm.

--- server.js
+++ server.js
@@ -1,27 +1,13 @@
-/*if(process.argv.length > 2)
-{
-    if(process.argv[2] == "start")
-    {
-        var forever = require("forever");
-        forever.start("server", {});
-    }
-    else if(process.argv[2] == "stop")
-    {
-        var forever = require("forever");
-        forever.stop("server");
-    }
-}*/
-
 server = require("express")();
+
+server.use(require("./tools/style.transpiler.js"));
+server.use(require("./tools/script.transpiler.js"));
+server.use(require("express").static("./source/"));
 
 server.get("/", function(request, response)
 {
     response.sendFile(__dirname + "/source/game.html");
 });
-
-server.use(require("./tools/style.transpiler.js"));
-server.use(require("./tools/script.transpiler.js"));
-server.use(require("express").static("./source/"));
 
 server.listen(1271, function()
 {
